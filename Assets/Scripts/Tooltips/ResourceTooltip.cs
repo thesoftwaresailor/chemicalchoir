6852692f49f55d039d15f708d34022bc6f5f45ee
thesoftwaresailor <81595888+thesoftwaresailor@users.noreply.github.com>
2022-01-23 00:28:12 -0500

    rework tooltips

--- Assets/Scripts/Tooltips/ResourceTooltip.cs
+++ Assets/Scripts/Tooltips/ResourceTooltip.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using System;
 
 public class ResourceTooltip : MonoBehaviour
 {
 
     public static ResourceTooltip instance;
 
-    public TextMeshProUGUI tooltip;
+    public TextMeshProUGUI resourceName;
+
+    public TextMeshProUGUI resourceDescription;
+
+    public Image tooltip;
 
     public Camera camera;
 
@@ -16,15 +22,21 @@ public class ResourceTooltip : MonoBehaviour
 
     public Vector3 offset;
 
+    public RectTransform parentTransform;
+
     public void ShowTooltip(Resource resource)
     {
         this.resource = resource;
         tooltip.enabled = true;
+        resourceName.enabled = true;
+        resourceDescription.enabled = true;
     }
 
     public void HideTooltip()
     {
         tooltip.enabled = false;
+        resourceName.enabled = false;
+        resourceDescription.enabled = false;
         this.resource = null;
     }
 
@@ -41,13 +53,18 @@ public class ResourceTooltip : MonoBehaviour
         {
             if (resource)
             {
-                tooltip.text = "Resource: \n" + resource.name;
-                transform.position = resource.gameObject.transform.position + offset;
-                transform.LookAt(transform.position + camera.transform.rotation * Vector3.forward, camera.transform.rotation * Vector3.up);
+                resourceName.text = resource.name;
+                double micro = Math.Round(resource.microTimer - (resource.microTimer - resource.microPhase), 1);
+                double macro = Math.Round(resource.macroTimer - (resource.macroTimer - resource.macroPhase), 1);
+                resourceDescription.text = "Minor Phase: " + micro + "\nMajor Phase: " + macro;
+                parentTransform.position = resource.gameObject.transform.position + offset;
+                parentTransform.LookAt(transform.position + camera.transform.rotation * Vector3.forward, camera.transform.rotation * Vector3.up);
             }
             else
             {
                 tooltip.enabled = false;
+                resourceName.enabled = false;
+                resourceDescription.enabled = false;
             }
         }
     }
